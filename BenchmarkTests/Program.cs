976339edd6a21eaa2a9ff9eac74878a7aa15f467
medcl <m@medcl.net>
2012-04-16 15:49:06 +0800

    fix license

--- BenchmarkTests/Program.cs
+++ BenchmarkTests/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ElasticSearch.Client;
+using ElasticSearch.Client.Config;
 
 namespace BenchmarkTests
 {
@@ -10,10 +11,12 @@ namespace BenchmarkTests
 	{
 		static void Main(string[] args)
 		{
-			var client = new ElasticSearchClient("localhost");
+			var client = new ElasticSearchClient("10.129.8.58",9500,TransportType.Thrift);
 			int failure = 0;
 
-			var data = "{\"book\": \"The Hitchhiker's Guide to the Galaxy\",\"chapter\": \"Chapter 11\",\"text1\": \"All the doors in this spaceship have a cheerful and sunny disposition. It is their pleasure to open for you, and their satisfaction to close again with the knowledge of a job well done.\"}";
+			var data = "{\"book\": \"The Hitchhiker's Guide to the Galaxy\"" +
+                ",\"id\":"+1+
+			           ",\"chapter\": \"Chapter 11\",\"text1\": \"All the doors in this spaceship have a cheerful and sunny disposition. It is their pleasure to open for you, and their satisfaction to close again with the knowledge of a job well done.\"}";
 			client.Index("benchmark_test", "default", Guid.NewGuid().ToString(), data);
 
 			int count = int.Parse(args[0]);
@@ -21,6 +24,11 @@ namespace BenchmarkTests
 			var begin = DateTime.Now;
 			for (var i = 0; i < count; i++)
 			{
+                data = "{\"book\": \"The Hitchhiker's Guide to the Galaxy\"" +
+                ",\"id\":" + i +
+                       ",\"chapter\": \"Chapter 11\",\"text1\": \"All the doors in this spaceship have a cheerful and sunny disposition. It is their pleasure to open for you, and their satisfaction to close again with the knowledge of a job well done.\"}";
+                client.Index("benchmark_test", "default", Guid.NewGuid().ToString(), data);
+
 				var response = client.Index("benchmark_test", "default", Guid.NewGuid().ToString(), data);
 				if (!response.Success)
 				{
